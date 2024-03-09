5aa9f0a65f5a2db178171bfc79fde2de9f14bd7c
Shreya Mohanty <shreya.official93@gmail.com>
2024-03-09 21:21:29 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, redirect, request, url_for
+import random
+import string
+import os
+
+app = Flask(__name__)
+shortened_urls = {}
+
+def generate_short_url(length=6):
+    chars = string.ascii_letters + string.digits
+    short_url = "".join(random.choice(chars) for _ in range(length))
+    return short_url
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    short_url = None
+
+    if request.method == 'POST':
+        long_url = request.form['long_url']
+        short_url = generate_short_url()
+
+        while short_url in shortened_urls:
+            short_url = generate_short_url()
+
+        shortened_urls[short_url] = long_url
+
+    return render_template('index.html', short_url=short_url)
+
+@app.route('/<short_url>')
+def redirect_url(short_url):
+    long_url = shortened_urls.get(short_url)
+    
+    if long_url:
+        return redirect(long_url)
+    else:
+        return "URL not found", 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
